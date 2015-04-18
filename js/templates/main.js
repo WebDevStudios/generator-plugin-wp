9bf00fcd37a14f34b3b954e0a2763a11f2918a1c
Justin Sternberg <justin@dsgnwrks.pro>
2015-04-18 19:56:22 -0400

    Tweak the setup of the main.js file

--- js/templates/main.js
+++ js/templates/main.js
@@ -5,26 +5,26 @@
  * Licensed under the GPLv2+ license.
  */
 
-/*jslint browser: true */
-/*global jQuery */
+window.{%= class_name %} = window.{%= class_name %} || {};
 
-window.<%= rc.classname %> = window.<%= rc.classname %> || (function(window, document, $, undefined) {
+( function( window, document, $, that, undefined ) {
+	'use strict';
 
-	var that = {},
-		$c   = {};
+	var $c   = {};
 
-	var init = function() {
+	that.init = function() {
 		that.cache();
 		that.bindEvents();
 	};
 
 	that.cache = function() {
-		$c.window = $(window);
-		$c.body   = $(document.body);
+		$c.window = $( window );
+		$c.body   = $( document.body );
 	};
 
 	that.bindEvents = function() {
 	};
 
-	$(init);
-})(window, document, jQuery);
+	$( that.init );
+
+} ) ( window, document, jQuery, {%= class_name %} );
