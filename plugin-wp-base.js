ca1b41479f444714ea84d84485962ebe8168553d
Camden Segal <camden.segal@gmail.com>
2015-05-18 11:09:51 -0400

    Fix indents on base class

--- plugin-wp-base.js
+++ plugin-wp-base.js
@@ -3,36 +3,36 @@ var yeoman = require('yeoman-generator');
 var updateNotifier = require('update-notifier');
 
 module.exports = yeoman.generators.Base.extend({
-	constructor: function () {
-		// Calling the super constructor is important so our generator is correctly set up
-		yeoman.generators.Base.apply(this, arguments);
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    yeoman.generators.Base.apply(this, arguments);
 
-		updateNotifier({
-	      pkg: require('./package.json')
-	    }).notify({defer: false});
-	},
+    updateNotifier({
+        pkg: require('./package.json')
+      }).notify({defer: false});
+  },
 
-	_wpClassify: function( s ) {
-		var words  = this._.words( s );
-		var result = '';
-		var word;
+  _wpClassify: function( s ) {
+    var words  = this._.words( s );
+    var result = '';
+    var word;
 
-		for ( var i = 0; i < words.length; i += 1 ) {
-			if ( this._.classify( words[i] ) ) {
-				result += this._.capitalize( words[i] );
-				if ( (i + 1) < words.length ) {
-					result += '_';
-				}
-			}
-		}
+    for ( var i = 0; i < words.length; i += 1 ) {
+      if ( this._.classify( words[i] ) ) {
+        result += this._.capitalize( words[i] );
+        if ( (i + 1) < words.length ) {
+          result += '_';
+        }
+      }
+    }
 
-		return result;
-	},
+    return result;
+  },
 
-	_wpClassPrefix: function( s ) {
-		var words = s.replace( /_/g, ' ' );
-		var letters = words.replace(/[a-z]/g, '');
-		var prefix = letters.replace(/\s/g, '');
-		return prefix + '_';
-	}
+  _wpClassPrefix: function( s ) {
+    var words = s.replace( /_/g, ' ' );
+    var letters = words.replace(/[a-z]/g, '');
+    var prefix = letters.replace(/\s/g, '');
+    return prefix + '_';
+  }
 });
