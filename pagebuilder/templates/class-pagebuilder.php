929cdc65dd19b625d7f005b155cebd67eaaa6b41
Chris Reynolds <chris@jazzsequence.com>
2015-10-12 09:13:32 -0600

    adds the template parts directory to the pagebuilder template parts stack

--- pagebuilder/templates/class-pagebuilder.php
+++ pagebuilder/templates/class-pagebuilder.php
@@ -36,5 +36,31 @@ class <%= classname %> {
 	 * @return void
 	 */
 	public function hooks() {
+		add_action( 'spb_init', array( $this, 'load_pagebuilder_parts' ) );
 	}
+
+	/**
+	 * Registers our parts directory in the Page Builder template stack.
+	 *
+	 * @since  <% version %>
+	 * @return void
+	 */
+	public function load_pagebuilder_parts() {
+		spb_register_template_stack( '<%= prefix %>_get_template_part_dir', 10 );
+	}
+}
+
+/**
+ * Register our plugin's template parts directory.
+ *
+ * @since  <% version %>
+ * @return string Absolute path to template parts directory.
+ */
+function <% prefix %>_get_template_part_dir() {
+	/**
+	 * Filters the absolute path of the template location.
+	 *
+	 * @param string The absolute path of the template package in use.
+	 */
+	return apply_fitlers( '<% prefix %>_get_template_part_dir', <% prefix %>()->path . '<% partsdir %>/' );
 }
