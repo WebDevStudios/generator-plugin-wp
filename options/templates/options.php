c51afc9913b21721a3aa7e57e048f9a2ae5bd90d
Gary Kovar <gkovar@gmail.com>
2017-01-05 21:13:51 -0500

    PHPCS fixes.

--- options/templates/options.php
+++ options/templates/options.php
@@ -7,7 +7,7 @@
  */
 
 <% if ( ! composer && ! options.nocmb2 ) {
-	%>require_once dirname(__FILE__) . '/../vendor/cmb2/init.php';<%
+	%>require_once dirname( __FILE__ ) . '/../vendor/cmb2/init.php';<%
 } %>
 
 /**
