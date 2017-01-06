b00df6180b79e8684f968d6d03de6acc56ef06df
Gary Kovar <gkovar@gmail.com>
2017-01-05 21:15:33 -0500

    PHPCS fixes.

--- cpt/templates/cpt.php
+++ cpt/templates/cpt.php
@@ -7,9 +7,9 @@
  */
 
 <% if ( ! composer ) {
-	%>require_once dirname(__FILE__) . '/../vendor/cpt-core/CPT_Core.php';<%
+	%>require_once dirname( __FILE__ ) . '/../vendor/cpt-core/CPT_Core.php';<%
 	if ( ! options.nocmb2 ) { %>
-<%		%>require_once dirname(__FILE__) . '/../vendor/cmb2/init.php';<%
+<%		%>require_once dirname( __FILE__ ) . '/../vendor/cmb2/init.php';<%
 	}
 } %>
 
