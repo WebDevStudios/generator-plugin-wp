6d406683e55eec3e99a846e3a938e409ea0021cb
Brad Parbs <brad@bradparbs.com>
2017-02-23 15:37:00 -0600

    Fix missing space in include test scaffold

--- include/templates/tests.php
+++ include/templates/tests.php
@@ -8,7 +8,7 @@ class <%= classname %>_Test extends WP_UnitTestCase {
 	}
 
 	function test_class_exists() {
-		$this->assertTrue( class_exists( '<%= classname %>') );
+		$this->assertTrue( class_exists( '<%= classname %>' ) );
 	}
 
 	function test_class_access() {
