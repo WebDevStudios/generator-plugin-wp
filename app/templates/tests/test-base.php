ab3b3889d97f9da4b7a277b85e4e2995361c737d
Brad Parbs <brad@bradparbs.com>
2017-03-01 16:40:16 -0600

    Add docs to main test

--- app/templates/tests/test-base.php
+++ app/templates/tests/test-base.php
@@ -1,22 +1,36 @@
 <?php
 /**
- * <%= mainclassname %>
+ * <%= mainclassname %>.
  *
- * @since <%= version %>
+ * @since   <%= version %>
  * @package <%= mainclassname %>
  */
 class <%= mainclassname %>_Test extends WP_UnitTestCase {
 
+	/**
+	 * Test if our class exists.
+	 *
+	 * @since  <%= version %>
+	 */
 	function test_class_exists() {
 		$this->assertTrue( class_exists( '<%= classname %>') );
 	}
 
+	/**
+	 * Test that our main helper function is an instance of our class.
+	 *
+	 * @since  <%= version %>
+	 */
 	function test_get_instance() {
 		$this->assertTrue( <%= prefix %>() instanceof <%= classname %> );
 	}
 
+	/**
+	 * Replace this with some actual testing code.
+	 *
+	 * @since  <%= version %>
+	 */
 	function test_sample() {
-		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
 }
