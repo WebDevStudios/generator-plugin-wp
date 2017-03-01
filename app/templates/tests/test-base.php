0faff34c512e714201cfe7d9aada838cdc577795
Brad Parbs <brad@bradparbs.com>
2017-03-01 14:30:16 -0600

    Docs + order for base test

--- app/templates/tests/test-base.php
+++ app/templates/tests/test-base.php
@@ -1,17 +1,22 @@
 <?php
-
-class BaseTest extends WP_UnitTestCase {
-
-	function test_sample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
-	}
+/**
+ * <%= mainclassname %>
+ *
+ * @since <%= version %>
+ * @package <%= mainclassname %>
+ */
+class <%= mainclassname %>_Test extends WP_UnitTestCase {
 
 	function test_class_exists() {
 		$this->assertTrue( class_exists( '<%= classname %>') );
 	}
-	
+
 	function test_get_instance() {
 		$this->assertTrue( <%= prefix %>() instanceof <%= classname %> );
+	}
+
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
 	}
 }
