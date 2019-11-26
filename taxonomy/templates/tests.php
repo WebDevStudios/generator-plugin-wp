dfe139689603ba842e21fc50ffa9c424adb547ee
David Romsey <david.romsey@webdevstudios.com>
2019-11-26 16:44:52 -0500

    Coding standards compliance for test: taxonomy

--- taxonomy/templates/tests.php
+++ taxonomy/templates/tests.php
@@ -5,6 +5,13 @@
  * @since   <%= version %>
  * @package <%= mainclassname %>
  */
+
+/**
+ * <%= taxonomyname %> Tests.
+ *
+ * @since   <%= version %>
+ * @package <%= mainclassname %>
+ */
 class <%= classname %>_Test extends WP_UnitTestCase {
 
 	/**
@@ -12,8 +19,8 @@ class <%= classname %>_Test extends WP_UnitTestCase {
 	 *
 	 * @since  <%= version %>
 	 */
-	function test_class_exists() {
-		$this->assertTrue( class_exists( '<%= classname %>') );
+	public function test_class_exists() {
+		$this->assertTrue( class_exists( '<%= classname %>' ) );
 	}
 
 	/**
@@ -21,7 +28,7 @@ class <%= classname %>_Test extends WP_UnitTestCase {
 	 *
 	 * @since  <%= version %>
 	 */
-	function test_class_access() {
+	public function test_class_access() {
 		$this->assertInstanceOf( '<%= classname %>', <%= rc.prefix %>()-><%= classslug %> );
 	}
 
@@ -30,7 +37,7 @@ class <%= classname %>_Test extends WP_UnitTestCase {
 	 *
 	 * @since  <%= version %>
 	 */
-	function test_taxonomy_exists() {
+	public function test_taxonomy_exists() {
 		$this->assertTrue( taxonomy_exists( '<%= taxonomyslug %>' ) );
 	}
 
@@ -39,7 +46,7 @@ class <%= classname %>_Test extends WP_UnitTestCase {
 	 *
 	 * @since  <%= version %>
 	 */
-	function test_sample() {
+	public function test_sample() {
 		$this->assertTrue( true );
 	}
 }
