a49c9c282722de6e8f3fd5309c4d2ac1db4a7408
David Romsey <david.romsey@webdevstudios.com>
2019-11-26 12:05:16 -0500

    issue-175 Fixes incorrect value for property names in generated CPT and taxonomy tests.

--- taxonomy/index.js
+++ taxonomy/index.js
@@ -40,6 +40,7 @@ module.exports = base.extend({
       this.taxonomyname    = this.pluginname + ' ' + this._.capitalize( this.name );
       this.classname  = this.rc.classprefix + this._wpClassify( this.name );
       this.slug       = this.rc.slug;
+      this.classslug      = this._.underscored( this._wpClassify( this.name ) );
       this.taxonomyslug    = this._.slugify( this.classname ).substr( 0, 20 );
       this.taxonomyprefix  = this._.underscored( this.taxonomyslug );
 
